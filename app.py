6ef836e9026bb80efd06f8fd952a91d6f4e2a3cf
Matan Samina <49943647+matanSamina@users.noreply.github.com>
2022-01-16 18:44:41 +0200

    creat form method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/unity", methods=["POST", "GET"])
+def unity():
+    if request.method == "POST":
+        user = request.form
+    else:
+        return "No request"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
